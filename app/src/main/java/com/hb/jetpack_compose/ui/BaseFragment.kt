093bb083d0f3e39709874ac470e8f27ef2c6d319
huanbin568 <huanbin568@gmail.com>
2022-09-26 10:48:59 +0800

    封装BaseFragment createComposeView方法，创建compose view

--- app/src/main/java/com/hb/jetpack_compose/ui/BaseFragment.kt
+++ app/src/main/java/com/hb/jetpack_compose/ui/BaseFragment.kt
@@ -1,30 +1,18 @@
 package com.hb.jetpack_compose.ui
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import com.google.accompanist.appcompattheme.AppCompatTheme
 
 
 abstract class BaseFragment : Fragment() {
-
-    @Composable
-    abstract fun createView()
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
+    //    参考setContent的实现
+    fun createComposeView(contentView: @Composable () -> Unit): ComposeView {
         val composeView = ComposeView(requireContext()).apply {
             setContent {
                 AppCompatTheme {
-                    createView()
+                    contentView.invoke()
                 }
             }
         }
