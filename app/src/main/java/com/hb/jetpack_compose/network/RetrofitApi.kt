d3639af4bca315672665d9410106fc89aa8c32cd
huanbin568 <huanbin568@gmail.com>
2022-09-28 14:14:20 +0800

    添加网络请求拦截器，打印网络请求log

--- app/src/main/java/com/hb/jetpack_compose/network/RetrofitApi.kt
+++ app/src/main/java/com/hb/jetpack_compose/network/RetrofitApi.kt
@@ -4,11 +4,21 @@ package com.hb.jetpack_compose.network
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 
 object RetrofitApi {
 
     const val BaseUrl = "https://www.wanandroid.com/"
+
+
+    private val okHttpClient: okhttp3.OkHttpClient by lazy {
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BASIC)
+        OkHttpClient.Builder().addInterceptor(httpLoggingInterceptor).build()
+    }
+
     private val retrofit: Retrofit by lazy {
         val contentType = "application/json".toMediaType()
         val json = Json {
@@ -17,6 +27,7 @@ object RetrofitApi {
         }
         Retrofit.Builder().apply {
             baseUrl(BaseUrl)
+            client(okHttpClient)
 //                .addConverterFactory(Json.asConverterFactory(contentType = ContentType))
                 .addConverterFactory(json.asConverterFactory(contentType = contentType))
         }.build()
