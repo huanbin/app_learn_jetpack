3e5b9344ffb3ac7e56997acdce85fec9127079e0
huanbin568 <huanbin568@gmail.com>
2022-09-29 15:21:48 +0800

    更新分页请求代码

--- app/src/main/java/com/hb/jetpack_compose/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/hb/jetpack_compose/ui/home/HomeViewModel.kt
@@ -5,11 +5,9 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.paging.Pager
-import androidx.paging.PagingConfig
 import androidx.paging.cachedIn
 import com.hb.jetpack_compose.network.RetrofitApi
-import com.hb.jetpack_compose.repository.ArticleDataSource
+import com.hb.jetpack_compose.repository.pagerFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.stateIn
@@ -23,16 +21,26 @@ class HomeViewModel : ViewModel() {
 
     val lazyListState = LazyListState()
 
-    val pager = Pager(
-        PagingConfig(
-            pageSize = ArticleDataSource.PageSize,
-//            prefetchDistance=10,
-//            maxSize用于避免浪费更多内存资源，enablePlaceholders是配合maxSize一起，在用户滑动列表超过maxSize之后，用户反向滑动时暂时显示null item
-            enablePlaceholders = true, maxSize = 200, initialLoadSize = 10
-        )
-    ) { ArticleDataSource() }.flow
-        //.flowOn(Dispatchers.IO)
-        .cachedIn(viewModelScope)
+    /*   val pager = Pager(
+           PagingConfig(
+               pageSize = ArticleDataSource.PageSize,
+   //            prefetchDistance=10,
+   //            maxSize用于避免浪费更多内存资源，enablePlaceholders是配合maxSize一起，在用户滑动列表超过maxSize之后，用户反向滑动时暂时显示null item
+               enablePlaceholders = true, maxSize = 200, initialLoadSize = 10
+           )
+       ) {
+           object : ArticleDataSource() {
+               override suspend fun Api.getData(page: Int, pageSize: Int): List<ArticleItemData> {
+                   return getHomeArticleList(page, pageSize).data.articleItemData
+               }
+           }
+       }.flow
+           //.flowOn(Dispatchers.IO)
+           .cachedIn(viewModelScope)*/
+
+    val pager = pagerFlow { page, pageSize ->
+        getHomeArticleList(page, pageSize).data.articleItemData
+    }.cachedIn(viewModelScope)
 
     val bannerStateFlow = flow {
         val homeBanner = RetrofitApi.getInstance().getHomeBanner().data
