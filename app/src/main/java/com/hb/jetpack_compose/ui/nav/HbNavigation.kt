7270b25ec88686450d6d5d990e5ae30788cff204
huanbin568 <huanbin568@gmail.com>
2022-10-25 11:31:42 +0800

    fix 打开文章详情页，底部Navigation仍然显示，遮挡文章详情内容

--- app/src/main/java/com/hb/jetpack_compose/ui/nav/HbNavigation.kt
+++ app/src/main/java/com/hb/jetpack_compose/ui/nav/HbNavigation.kt
@@ -5,46 +5,55 @@ import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
-import com.hb.jetpack_compose.ui.Screen
 
 
 val items = listOf(Screen.Home, Screen.Topic, Screen.Setting)
 
 @Composable
 fun HbNavigation(navHostController: NavHostController) {
-    BottomNavigation {
-        val navBackStackEntry by navHostController.currentBackStackEntryAsState()
+    val navBackStackEntry by navHostController.currentBackStackEntryAsState()
 
-        val currentDestination = navBackStackEntry?.destination
+    val currentDestination = navBackStackEntry?.destination
 
-        items.forEach { item ->
-            BottomNavigationItem(icon = {
-                Icon(
-                    painter = painterResource(id = item.drawableRes),
-                    contentDescription = item.route
-                )
-            },
-                label = {
-                    Text(text = item.route)
+    val showBottomNavigation by remember(currentDestination) {
+        derivedStateOf { items.contains(items.find { it.route == currentDestination?.route }) }
+    }
+
+    if (showBottomNavigation) {
+
+        BottomNavigation {
+
+            items.forEach { item ->
+                BottomNavigationItem(icon = {
+                    Icon(
+                        painter = painterResource(id = item.drawableRes),
+                        contentDescription = item.route
+                    )
                 },
-                selected = currentDestination?.hierarchy?.any { it.route == item.route } == true,
-                onClick = {
-                    navHostController.navigate(item.route) {
-                        popUpTo(navHostController.graph.findStartDestination().id) {
-                            saveState = true
+                    label = {
+                        Text(text = item.route)
+                    },
+                    selected = currentDestination?.hierarchy?.any { it.route == item.route } == true,
+                    onClick = {
+                        navHostController.navigate(item.route) {
+                            popUpTo(navHostController.graph.findStartDestination().id) {
+                                saveState = true
+                            }
+
+                            launchSingleTop = true
+
+                            restoreState = true
                         }
-
-                        launchSingleTop = true
-
-                        restoreState = true
-                    }
-                })
+                    })
+            }
         }
     }
 }
